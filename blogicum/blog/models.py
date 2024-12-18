2789c5678e35dc497e6b5d08619a783b3e184a87
Danila Olyanin <danilaolyanin@yandex.ru>
2024-12-18 21:52:11 +0300

    Finish

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -5,39 +5,76 @@ User = get_user_model()
 
 
 class BaseModel(models.Model):
-    is_published = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField(
+        'Опубликовано',
+        default=True,
+        help_text='Снимите галочку, чтобы скрыть публикацию.',
+    )
+    created_at = models.DateTimeField('Добавлено', auto_now_add=True)
 
     class Meta:
         abstract = True
 
 
 class Category(BaseModel):
-    title = models.CharField(max_length=256)
-    description = models.TextField()
-    slug = models.SlugField(unique=True)
+    title = models.CharField('Заголовок', max_length=256)
+    description = models.TextField('Описание')
+    slug = models.SlugField(
+        'Идентификатор',
+        unique=True,
+        help_text='Идентификатор страницы для URL; '
+                  'разрешены символы латиницы, цифры, дефис и подчёркивание.'
+    )
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.title
 
 
 class Location(BaseModel):
-    name = models.CharField(max_length=256)
+    name = models.CharField('Название места', max_length=256)
+
+    class Meta:
+        verbose_name = 'местоположение'
+        verbose_name_plural = 'Местоположения'
+
+    def __str__(self):
+        return self.name
 
 
 class Post(BaseModel):
-    title = models.CharField(max_length=256)
-    text = models.TextField()
-    pub_date = models.DateTimeField()
+    title = models.CharField('Заголовок', max_length=256)
+    text = models.TextField('Текст')
+    pub_date = models.DateTimeField(
+        'Дата и время публикации',
+        help_text='Если установить дату и время в будущем — '
+                  'можно делать отложенные публикации.'
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
+        verbose_name='Автор публикации',
     )
     location = models.ForeignKey(
         Location,
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
+        verbose_name='Местоположение',
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
         null=True,
+        verbose_name='Категория',
     )
+
+    class Meta:
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
+
+    def __str__(self):
+        return self.title
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,29 +1,52 @@
+from django.db.models import Q
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+
+from .models import Post, Category
 
 
 def index(request):
     template = 'blog/index.html'
-    context = {'posts': posts}
+    post_list = Post.objects.filter(
+        Q(is_published=True)
+        & Q(category__is_published=True)
+        & Q(pub_date__lte=timezone.now())
+    ).order_by('-pub_date').all()[0:5]
+
+    context = {'post_list': post_list}
+
     return render(request, template, context)
 
 
 def post_detail(request, post_id):
-    requested_post = None
-
-    for post in posts:
-        if post['id'] == post_id:
-            requested_post = post
-
-    if requested_post is None:
-        raise Http404
-
     template = 'blog/detail.html'
-    context = {'post': requested_post}
+
+    post = get_object_or_404(Post, pk=post_id)
+
+    if (not post.is_published
+            or not post.category.is_published
+            or post.pub_date > timezone.now()):
+        raise Http404()
+
+    context = {'post': post}
+
     return render(request, template, context)
 
 
 def category_posts(request, category_slug):
     template = 'blog/category.html'
-    context = {'category': category_slug}
+    category = get_object_or_404(Category, slug=category_slug)
+
+    if not category.is_published:
+        raise Http404()
+
+    post_list = Post.objects.filter(
+        Q(category=category)
+        & Q(is_published=True)
+        & Q(pub_date__lte=timezone.now())
+    ).all()
+
+    context = {'category': category, 'post_list': post_list}
+
     return render(request, template, context)
